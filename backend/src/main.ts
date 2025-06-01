6da0737a87dc3f094d310a5beb2262492fcbe7a3
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-06-01 17:57:53 +0200

    add openapi client generation for frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,9 @@ async function bootstrap() {
       .setVersion('1.0')
       .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, documentFactory);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    jsonDocumentUrl: "openapi.json"
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
